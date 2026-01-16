4ac8949c492a86c4ecc4fd2b3c6a27f03d8fbfd7
javiergonzalez-code <javier.gonzalez.contact1016@gmail.com>
2026-01-16 13:43:30 -0600

    LOGOUT

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-// La raíz de tu sitio ahora apunta al método index del controlador
+
 Route::get('/', [AuthController::class, 'index'])->name('login');
 
 // Ruta para recibir los datos del formulario (POST)
@@ -11,3 +11,5 @@ Route::post('/login', [AuthController::class, 'login'])->name('login.post');
 
 // Ruta para la página interna
 Route::get('/logeados', [AuthController::class, 'logeados'])->name('logeados');
+
+Route::post('/logout', [Logoutcontroller::class, 'destroy'])->middleware('auth');
