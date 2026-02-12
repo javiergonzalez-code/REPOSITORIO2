7193f872a6d30061049378e3f5b9c25cedea2348
javiergonzalez-code <javier.gonzalez.contact1016@gmail.com>
2026-02-12 10:19:00 -0600

    CAMBIOS DISEÑO

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        // AGREGA ESTA LÍNEA PARA ELIMINAR EL MIDDLEWARE QUE FALLA:
         $middleware->remove(\RealRashid\SweetAlert\ToSweetAlert::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
