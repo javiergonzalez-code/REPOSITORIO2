cf8f1ad3343ecb0ea47f6dc0c56fd25730b93fdb
javiergonzalez-code <javier.gonzalez.contact1016@gmail.com>
2026-02-17 13:26:02 -0600

    CORRECCIONES DISEÑO, NAVEGACION EN BARRA DE NAVEGACION, PAGINA DE BIENVENIDA EN VISTA DED SUPERUSUARIO

--- resources/views/vendor/backpack/ui/inc/menu_items.blade.php
+++ resources/views/vendor/backpack/ui/inc/menu_items.blade.php
@@ -1,2 +1,37 @@
-{{-- This file is used for menu items by any Backpack v7 theme --}}
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
+{{-- Dashboard --}}
+<li class="nav-item">
+    <a class="nav-link" href="{{ backpack_url('dashboard') }}">
+        <i class="la la-home nav-icon"></i> {{ trans('backpack::base.dashboard') }}
+    </a>
+</li>
+
+{{-- Sección de Administración de Accesos (Spatie + Backpack) --}}
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" role="button" aria-expanded="true">
+        <i class="la la-users nav-icon"></i> Autenticación
+    </a>
+    <div class="dropdown-menu" data-bs-popper="static">
+        <a class="dropdown-item" href="{{ backpack_url('user') }}">
+            <i class="la la-user nav-icon"></i> Usuarios
+        </a>
+        <a class="dropdown-item" href="{{ backpack_url('role') }}">
+            <i class="la la-id-badge nav-icon"></i> Roles
+        </a>
+        <a class="dropdown-item" href="{{ backpack_url('permission') }}">
+            <i class="la la-key nav-icon"></i> Permisos
+        </a>
+    </div>
+</li>
+
+{{-- Otros Módulos --}}
+<li class="nav-item">
+    <a class="nav-link" href="{{ backpack_url('archivo') }}">
+        <i class="la la-file nav-icon"></i> Archivos
+    </a>
+</li>
+
+<li class="nav-item">
+    <a class="nav-link" href="{{ backpack_url('log') }}">
+        <i class="la la-history nav-icon"></i> Logs
+    </a>
+</li>
